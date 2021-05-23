97b009a13401b1dec155326102a77ee3a58d6769
Shamar Samuels <shamarsamuelsbusiness@gmail.com>
2021-05-23 13:28:36 -0400

    Traveling Salesman Solution

--- main.cpp
+++ main.cpp
@@ -4,63 +4,122 @@
 #include <stdlib.h>
 #include <time.h>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+int POINTS = 100;
+int POP_SIZE = 100;
+int GENERATIONS = 10000;
+int VALUE_MAX = 1000;
+int ROUTES = (0.5)*(POINTS-1)*(POINTS);
+
 #include "point.h"
 
-int POINTS = 10;
-int POP_SIZE = 100;
+void create_distribution(vector<Path*>& parents, vector<double>& distribution, Path*& best_path, Path*& worst_path){
+    worst_path = parents[0]; 
+    best_path = parents[0];
+    int worst = parents[0]->path_value;
+    int best = parents[0]->path_value;
+    int total = 0;
+    for(int i = 0; i < POP_SIZE; i++){
+        total += parents[i]->path_value;
+        distribution.push_back(parents[i]->path_value);
+        if(parents[i]->path_value > worst){
+            worst_path = parents[i];
+            worst = parents[i]->path_value;
+        }
+        if(parents[i]->path_value < best){
+            best_path = parents[i];
+            best = parents[i]->path_value;
+        }
+    }
+    int new_total = 0;
+    for(int i = 0; i < POP_SIZE; i++){
+        distribution[i] = worst - distribution[i];
+        new_total += distribution[i];
+    }
+    for(int i = 0; i < POP_SIZE; i++){
+        distribution[i] = distribution[i] / new_total;
+    }
+}
+
+Path* random_distribution(vector<Path*>& parents, vector<double>& distribution){
+    double rand_num = rand() / double(RAND_MAX);
+    double sum = 0;
+    for(int i = 0; i < POP_SIZE; i++){
+        sum += distribution[i];
+        if(rand_num < sum){
+            return parents[i];
+        }
+    }
+    return parents.back();
+}
 
 int main(){
-
     srand(time(0));
 
     vector<Point*> points;
     for(int i = 0; i < POINTS; i++){
         points.push_back(new Point(i));
     }
+    int total = 0;
     for(int i = 0; i < POINTS; i++){
         for(int x = i+1; x < POINTS; x++){
-            points[i]->add_route(points[x], (rand() % 100) + 1);
+            int value = (rand() % VALUE_MAX) + 1;
+            total += value;
+            points[i]->add_route(points[x], value);
         }
     }
-    for(int i = 0; i < POINTS; i++){
-        points[i]->print_routes();
-    }
+    
+    double value_per_route = (double) total / ROUTES;
+    cout << "Routes: " << ROUTES << " AVG: " << value_per_route << endl;
 
-    vector<Point*> best_path;
-    int best_path_value = 0;
+    vector<Path*> parents; 
 
-    int count = 0;
-    while(count < POP_SIZE){
-        vector<Point*> path;
-        vector<Point*> points_left = points;
-        while(points_left.size() > 0){
-            int index = rand() % points_left.size();
-            path.push_back(points_left[index]);
-            points_left.erase(points_left.begin() + index);
+    int generation = 0;
+    if(generation == 0){
+        int count = 0;
+        while(count < POP_SIZE){
+            Path* path = new Path;
+            vector<Point*> points_left = points;
+            while(points_left.size() > 0){
+                int index = rand() % points_left.size();
+                path->add_point(points_left[index]);
+                points_left.erase(points_left.begin() + index);
+            }
+            parents.push_back(path);
+            count++;
         }
-        int path_value = 0;
-        // cout << "Path: < ";
-        for(int i = 0; i < path.size() - 1; i++){
-            // cout << path[i]->point << "->";
-            path_value += path[i]->get_value(path[i+1]);
-        }
-        // cout << path.back()->point << " >" << endl;
-        // cout << "Path Value: " << path_value << endl;
-        
-        if(path_value > best_path_value){
-            best_path_value = path_value;
-            best_path = path;
-        }
-        count++;
     }
-
-    cout << "Best Path: < ";
-    for(int i = 0; i < best_path.size() - 1; i++){
-        cout << best_path[i]->point << "->";
+    Path best_generation_path = *parents[0];
+    best_generation_path.calculate_path_value();
+    while(generation < GENERATIONS){
+        vector<double> distribution;
+        for(int i = 0; i < POP_SIZE; i++){
+            parents[i]->calculate_path_value();
+        }
+        sort(parents.begin(), parents.end(), path_sort);
+        Path* best_path, *worst_path;
+        create_distribution(parents, distribution, best_path, worst_path);   
+        if(best_generation_path.path_value > best_path->path_value){
+            best_generation_path = *best_path;
+        }
+        // cout << "Generation: " << generation << " Best: " << best_path->path_value << " Worst: " << worst_path->path_value << endl;
+        vector<Path*> new_generation;
+        int count = 0;
+        while(count < POP_SIZE){
+            Path* parent = random_distribution(parents, distribution);
+            Path* new_child = new Path(*parent);
+            if(rand() % 100 < 20){
+                new_child->swap_mutation();
+            }
+            new_generation.push_back(new_child);
+            count++;
+        } 
+        for(int i = 0; i < POP_SIZE; i++) delete parents[i];
+        generation++;
+        parents = new_generation;
     }
-    cout << best_path.back()->point << " >" << endl;
-    cout << "Best Path Value: " << best_path_value << endl;
+    best_generation_path.print_path();
 }
--- point.h
+++ point.h
@@ -3,26 +3,74 @@
 
 class Point {
 public:
-Point(int _point) : point(_point) {}
-void add_route(Point* end, int value){
-    pair<map<Point*, int>::iterator, bool> ret = routes.insert(make_pair(end, value));
-    if(ret.second){
-        end->add_route(this, value);
+    Point(int _point) : point(_point) {}
+    void add_route(Point* end, int value){
+        pair<map<Point*, int>::iterator, bool> ret = routes.insert(make_pair(end, value));
+        if(ret.second){
+            end->add_route(this, value);
+        }
     }
-}
-int get_value(Point* next){
-    return routes[next];
-}
-void print_routes(){
-    cout << point << ": < "; 
-    for(map<Point*, int>::iterator route_itr = routes.begin(); route_itr != routes.end(); route_itr++){
-        cout << route_itr->first->point << '=' << route_itr->second << ' ';
+    int get_value(Point* next){
+        return routes[next];
     }
-    cout << ">" << endl; 
+    void print_routes(){
+        cout << point << ": < "; 
+        for(map<Point*, int>::iterator route_itr = routes.begin(); route_itr != routes.end(); route_itr++){
+            cout << route_itr->first->point << '=' << route_itr->second << ' ';
+        }
+        cout << ">" << endl; 
 
-}
-map<Point*, int> routes;
-int point;
+    }
+    map<Point*, int> routes;
+    int point;
 };
+
+class Path {
+public:
+    Path(){
+        path_value = 0;
+    }
+    Path(const Path& p){
+        path = p.path;
+        path_value = p.path_value;
+    }
+    Path* operator=(const Path& p){
+        path = p.path;
+        path_value = p.path_value;
+        return this;
+    }
+    void add_point(Point* next){
+        path.push_back(next);
+    }
+    int calculate_path_value(){
+        path_value = 0;
+        for(int i = 0; i < path.size() - 1; i++){
+            path_value += path[i]->get_value(path[i+1]);
+        }
+        return path_value;
+    }
+    void swap_mutation(){
+        int i = rand() % path.size();
+        int x = rand() % path.size();
+        swap(path[i], path[x]);
+    }
+    void print_path(){
+        cout << "Path: < "; 
+        for(int i = 0; i < path.size(); i++){
+            cout << path[i]->point << ' ';
+        }
+        cout << "> Value: " << path_value << " AVG: " << (float) path_value / POINTS << endl;
+    }
+    bool operator<(const Path& b){
+        return path_value > b.path_value;
+    }
+    vector<Point*> path;
+    int path_value;
+};
+
+bool path_sort(Path* a, Path* b){
+    return a->path_value > b->path_value;
+}
+
 
 #endif
